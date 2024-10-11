5942d842bfba054cfe8d42843915bfcf922fe05b
ashan_m <ashan_m@epiclanka.net>
2024-10-11 10:07:08 +0530

    initial commit

---
+++ SpringBoot-Questions/src/main/java/com/sp/question/Main.java
@@ -0,0 +1,15 @@
+package com.sp.question;
+
+/**
+ * Title: Default (Template) Project
+ * Description: ${NAME} Class
+ * Created by Ashan Sandeep on 10/11/2024
+ * Email: ashan_m@epiclanka.net
+ * Company: Epic Lanka (Pvt) Ltd.
+ * Java Version: 17
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
